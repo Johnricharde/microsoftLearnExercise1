043359554d06a042db5a9e3c13d1f4c8491af302
John Richard Espedal <117681128+Johnricharde@users.noreply.github.com>
2023-12-06 10:24:09 +0100

    Add project files.

---
+++ microsoftLearnExercise1/Program.cs
@@ -0,0 +1,27 @@
+﻿namespace MicrosoftLearnExercise1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string[] values = { "12,3", "45", "ABC", "11", "DEF" };
+
+            string messageResult = "";
+            decimal numberResult = 0;
+
+            foreach (string value in values)
+            {
+                if (decimal.TryParse(value, out decimal parsedValue))
+                    numberResult += parsedValue;
+                else
+                {
+                    messageResult += value;
+                }
+            }
+
+            Console.WriteLine($"Message: {messageResult}");
+            Console.WriteLine($"Total: { numberResult}");
+        }
+    }
+}
+
